37b5d9274745b40aa5ba5e1f1ceb01ab1c4d171c
Jake Gillis <jaques.gillis@gmail.com>
2015-11-07 13:30:13 -0500

    Added dynamic item list to laptop-size page

--- js/script.js
+++ js/script.js
@@ -17,12 +17,16 @@ $(document).ready(function() {
 		$('#Total_row').attr("placeholder", total2).blur();
 	});
 	
+	
 	$('#Addbtn').click(function() {
-		var item = $('#SampleProduct').val();
-		var price = $('#Price').val();
-		var quant = $('#quantity').val();
+		var item = $('.SampleProduct').val();
+		var price = $('.Price').val();
+		var quant = $('.quantity').val();
 		var total = price * quant;
-		var bigtext = string.concat("<div class='form-group'>\
+		var id = item;
+		var string = "";
+		var bigtext = string.concat("<nobr>\
+					<div class='bigtext form-group' id='", id, "' >\
 					<div class='edit_buttons'><button class='little_button'><i class='fa fa-trash'></i></button><button class='little_button'><i class='fa fa-pencil'></i></button><button class='little_button'><i class='fa fa-check'></i></button></div>\
 					<label class='sr-only'>Product description</label>\
 					<div class='input-group col-sm-4'>\
@@ -42,9 +46,16 @@ $(document).ready(function() {
 						<input type='text' class='form-control' id='Total' placeholder='", total,"' readonly>\
 					</div>\
 				</div>\
-			</form>\
 			</nobr>");
+
+
 		$('#biglist').append(
 			bigtext);
+	});
+
+	$('#Addbtn2').click(function() {
+		var testtext = "<div><p>This is a test</p></div>";
+		$('#small_list').append(
+			testtext);
 	});
 });
